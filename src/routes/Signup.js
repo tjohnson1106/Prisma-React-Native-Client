38891013e8ad6c6d4ce44abd9ecc7860298b4f0a
Thomas Johnson <thomasjohnson@Thomass-iMac.local>
2018-03-27 14:33:17 -0400

    added values variable

--- src/routes/Signup.js
+++ src/routes/Signup.js
@@ -1,13 +1,22 @@
 import React, { Component } from "react";
-import { Text, TextInput, View } from "react-native";
+import { Text, TextInput, View, Button, StyleSheet } from "react-native";
 
 class Signup extends Component {
   state = {
-    values: {},
+    values: {
+      name: "",
+      username: "",
+      password: ""
+    },
     errors: {},
     isSubmitting: false
   };
+
+  submit = () => {};
+
   render() {
+    const { values: { name, username, password } } = this.state;
+
     return (
       <View
         style={{
@@ -18,31 +27,31 @@ class Signup extends Component {
         }}
       >
         <View style={{ width: 200 }}>
+          <TextInput value={name} style={styles.field} placeholder="name" />
           <TextInput
-            style={{
-              borderBottomWidth: 1,
-              fontSize: 20
-            }}
-            placeholder="name"
-          />
-          <TextInput
-            style={{
-              borderBottomWidth: 1,
-              fontSize: 20
-            }}
+            value={username}
+            style={styles.field}
             placeholder="email"
           />
           <TextInput
-            style={{
-              borderBottomWidth: 1,
-              fontSize: 20
-            }}
+            value={password}
+            style={styles.field}
             placeholder="password"
           />
+          <Button title="Create Account" onPress={this.submit} />
         </View>
       </View>
     );
   }
 }
+
+const styles = StyleSheet.create({
+  field: {
+    borderBottomWidth: 1,
+    fontSize: 20,
+    marginBottom: 15,
+    height: 35
+  }
+});
 
 export default Signup;
